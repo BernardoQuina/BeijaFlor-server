08481d23b980e10bbef134fd2a5fbccfda7f8360
Bernardo-Quina <bernardoquina@gmail.com>
2021-06-10 09:58:50 +0100

    Atualização da encomenda: email

--- src/schema/Order/mutations.ts
+++ src/schema/Order/mutations.ts
@@ -1,5 +1,7 @@
 import { mutationField, stringArg, nonNull, list } from 'nexus'
+import { atualizaçãoEncomenda } from '../../emails/atualizaçãoEncomenda'
 import { isAuth } from '../../util/isAuth'
+import { sendEmail } from '../../util/sendEmail'
 
 export const createOrder = mutationField('createOrder', {
   type: 'Order',
@@ -118,9 +120,75 @@ export const orderState = mutationField('orderState', {
       throw new Error('A encomenda que pretende editar não existe.')
     }
 
-    return context.prisma.order.update({
+    const updatedOrder = await context.prisma.order.update({
       where: { id: whereId },
       data: { state },
     })
+
+    const orderAddress = await context.prisma.address.findUnique({
+      where: { id: orderExists.addressId },
+    })
+
+    if (!orderAddress) {
+      throw new Error('Ocorreu um problema ao processar a morada da encomenda.')
+    }
+
+    const address = `${orderAddress.street}, ${orderAddress.numberAndBlock}, ${orderAddress.postal} ${orderAddress.zone}`
+
+    const stateDiagram = `
+    <div style="height: 100%; width: 30%;${
+      updatedOrder.state === 'Em processamento'
+        ? ' background-color: #f5fae8;'
+        : ''
+    } border-radius: 8px; margin-right:auto; display: flex;${
+      updatedOrder.state === 'Em processamento'
+        ? ''
+        : ' border: 1px solid #c5c5c5;'
+    }">
+          <h5 style="margin: auto; text-align: center; color: #6b705c;${
+            updatedOrder.state === 'Em processamento'
+              ? ''
+              : ' font-weight: 400;'
+          }">Em processamento</h5>
+        </div>
+    <div style="height: 100%; width: 30%;${
+      updatedOrder.state === 'Em distribuição'
+        ? ' background-color: #f5fae8;'
+        : ''
+    } border-radius: 8px; margin-right:auto; display: flex;${
+      updatedOrder.state === 'Em distribuição'
+        ? ''
+        : ' border: 1px solid #c5c5c5;'
+    }">
+          <h5 style="margin: auto; text-align: center; color: #6b705c;${
+            updatedOrder.state === 'Em distribuição' ? '' : ' font-weight: 400;'
+          }">Em distribuição</h5>
+        </div>
+    <div style="height: 100%; width: 30%;${
+      updatedOrder.state === 'Entregue' ? ' background-color: #f5fae8;' : ''
+    } border-radius: 8px; margin-right:auto; display: flex;${
+      updatedOrder.state === 'Entregue' ? '' : ' border: 1px solid #c5c5c5;'
+    }">
+          <h5 style="margin: auto; text-align: center; color: #6b705c;${
+            updatedOrder.state === 'Entregue' ? '' : ' font-weight: 400;'
+          }">Entregue</h5>
+        </div>
+    `
+
+    const html = atualizaçãoEncomenda(
+      orderExists.id,
+      user.name,
+      updatedOrder.state.toLowerCase(),
+      stateDiagram,
+      orderAddress.completeName,
+      address
+    )
+    await sendEmail(
+      user.email,
+      `Atualização da encomenda: ${updatedOrder.state}`,
+      html
+    )
+
+    return updatedOrder
   },
 })
